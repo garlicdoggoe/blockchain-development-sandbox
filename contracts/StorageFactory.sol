ddebcc440cf65af21ed356f00579736e9d00c187
garlicdoggoe <garlicspoofiyyy@gmail.com>
2025-05-30 10:15:28 +0800

    simplify: store function

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -13,8 +13,7 @@ contract StorageFactory {
     }
 
     function sfStore(uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public {
-        SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
-        mySimpleStorage.store(_newSimpleStorageNumber);
+        listOfSimpleStorageContracts[_simpleStorageIndex].store(_newSimpleStorageNumber);
     }
 
     function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
