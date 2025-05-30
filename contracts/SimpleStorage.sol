71f3153fc84edf946a0c29e8986ac2a900245f56
garlicdoggoe <garlicspoofiyyy@gmail.com>
2025-05-30 10:06:49 +0800

    add: initial solidity smart contract development

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19; // version
+
+contract SimpleStorage {
+    uint256 myFavoriteNumber;
+
+    // uint256[] listOfFavoriteNUmbers;
+
+    struct Person{
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    Person[] public listOfPeople;
+
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    // Person public pat = Person({favoriteNumber: 7, name: "Pat"});
+
+    function store(uint256 _favoriteNumber) public {
+        myFavoriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns (uint256) {
+        return myFavoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push( Person(_favoriteNumber, _name) );
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    } 
+}
